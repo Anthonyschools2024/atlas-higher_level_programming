a8f4f47e6a00af0ed69bece5d018ef1ece3e6ea1
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-09-03 21:56:55 +0000

    To uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+!/usr/bin/python3
+
+def uppercase(str):
+    """
+    uppercase(str: string)
+    :param str: string
+    :return: string in caps
+    """
+    result = ""
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            result += "{:c}".format(ord(char) - 32)
+        else:
+            result += "{:c}".format(ord(char))
+    print(result)

6cdb088d77b376ec5bd00f7039ebcd36a11af985
root <Anthony.Kimble@atlasschool.com>
2024-09-10 23:57:51 +0000

    square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class name square
+"""
+
+
+class Square:
+    """
+    private instanced attribute __size
+    """
+    def __init__(self, size):
+        self.__size = size

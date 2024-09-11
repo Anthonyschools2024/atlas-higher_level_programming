0d286d7f67180ceff5572436fbf78692ca13d3d0
root <Anthony.Kimble@atlasschool.com>
2024-09-11 03:49:42 +0000

    third attempt of getting this code right

--- python-test_driven_development/6-max_integer_test.py
+++ python-test_driven_development/6-max_integer_test.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
-"""
-unittests for max integer
+"""Unittest for max_integer([..])
 """
 import unittest
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def test_positive_integers(self):
+    """unittest for max_integer"""
+
+    def test_max_integer(self):
+        self.assertIs(list, list)
+        self.assertRaises(TypeError, max_integer("string"))
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
+        self.assertEqual(max_integer([-8, -6, -2, 0]), 0)
+        self.assertEqual(max_integer([1, 2, 3, -4]), 3)
+        self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([]), None)
 
-    def test_negative_integers(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+    if __name__ == "__main__":
+        """run the main function"""
 
-    def test_mixed_integers(self):
-        self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
 
-    def test_single_element(self):
-        self.assertEqual(max_integer([7]), 7)
-
-    def test_empty_list(self):
-        self.assertIsNone(max_integer([]))
-
-    def test_none(self):
-        with self.assertRaises(TypeError):
-            max_integer(None)
-
-    def test_not_integer_elements(self):
-        with self.assertRaises(TypeError):
-            max_integer([1, "two", 3])
-
-    def test_not_integer_elements(self):
-        with self.assertRaises(TypeError):
-            max_integer([1, "two", 3])
-
-    def test_floats(self):
-        self.assertEqual(max_integer([1.5, 2.7, 3.8, 4.2]), 4.2)
-
-    def test_repeat_max(self):
-        self.assertEqual(max_integer([1, 2, 3, 3, 2, 1]), 3)
-
-    def test_large(self):
-        self.assertEqual(max_integer([1000000, 2000000, 3000000]), 3000000)
-if __name__ == '__main__':
-    unittest.main()
+        unittest.main()

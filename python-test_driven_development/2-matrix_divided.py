77eb9d06b0d12a5e5fc94a6966ac62a423cd7a38
root <Anthony.Kimble@atlasschool.com>
2024-09-11 03:01:19 +0000

    divide a matrix file update

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""This defines a function that divides all elements of a matrix"""
+"""
+This module provides a function to add two ints
+"""
 
 
-def matrix_divided(matrix, div):
-    """Function that divides all elements of a matrix"""
-    if not all(
-        isinstance(row, list) and all(
-            isinstance(element, (int, float)) for element in row
-        ) for row in matrix
-    ):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
-    if any(len(row) != len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    new_matrix = [
-        [round(element / div, 2) for element in row] for row in matrix
-    ]
-    return new_matrix
+def add_integer(a, b=98):
+    """
+    Adds two ints or floats after converting floats to ints
+
+    Args:
+    a (int or float) the first number to add
+    b (int or float) The second number to add defaults to 98]
+
+    Raises:
+    TypeError: if either 'a' or 'b' is not an integer
+
+    Returns:
+    int: the of 'a' and 'b'.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)

506bdab97c566c7470c66ba1bdcaa02fb246469a
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-09-23 20:32:47 -0500

    base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""class Base"""
+from json import dumps
+
+
+class Base:
+    """init class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialization"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)

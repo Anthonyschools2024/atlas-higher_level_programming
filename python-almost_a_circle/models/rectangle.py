479ac055297d71782c9e17500d1566c170287b9e
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-09-24 20:45:13 -0500

    update#1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,31 +19,31 @@ class Rectangle(Base):
             id (int): Unique identifier for the rectangle (default=None)
         """
         super().__init__(id)
-        
+
         # Validate width
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
-        
+
         # Validate height
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height <= 0:
             raise ValueError("height must be > 0")
-        
+
         # Validate x
         if type(x) is not int:
             raise TypeError("x must be an integer")
         if x < 0:
             raise ValueError("x must be >= 0")
-        
+
         # Validate y
         if type(y) is not int:
             raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
-        
+
         self.width = width
         self.height = height
         self.x = x
@@ -86,7 +86,7 @@ class Rectangle(Base):
         # Print empty lines for the y-coordinate
         for _ in range(self.y):
             print()
-        
+
         # Print the rectangle with spaces for the x-coordinate
         for _ in range(self.height):
             print(' ' * self.x + '#' * self.width)
@@ -95,16 +95,31 @@ class Rectangle(Base):
         """Return string representation of the rectangle"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update attributes of the rectangle"""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            # Update attributes using kwargs
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
    

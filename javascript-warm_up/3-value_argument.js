87a519bee12b761ed04d9cb4ac7d3a9a74de073a
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-11-19 16:44:39 -0600

    had to delete comment

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,16 +1,6 @@
 #!/usr/bin/node
-/*
-  Write a script that prints the first argument passed to it:
-
-  If no arguments are passed to the script, print “No argument”
-  You must use console.log(...) to print all output
-  You are not allowed to use var
-  You are not allowed to use length
-*/
-
-const myArgs = process.argv.slice(2);
-if (myArgs[0]) {
-  console.log(myArgs[0]);
-} else {
+if (process.argv[2] === undefined) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }

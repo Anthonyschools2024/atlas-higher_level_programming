e41fa3919566b9235dae6c383b556ee4fced2376
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-11-19 22:04:35 -0600

    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const n = parseInt(process.argv[2]);
+console.log(factorial(n)); 1

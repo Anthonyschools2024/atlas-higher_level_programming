40092fde3d672ba932f421a581ee974675e61d89
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-11-24 22:19:26 -0600

    had to fix the url

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 const fetch = require('node-fetch');
 
-const apiUrl = process.argv[2]; 
-const wedgeAntillesId = '18';
+const apiUrl = process.argv[2] || 'https://swapi-api.hbtn.io/api/films/'; // Default URL added
+const wedgeAntillesId = 18; // Corrected to numeric ID
 
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
     const films = data.results;
     const wedgeAntillesCount = films.reduce((count, film) => {
+      // Use numeric ID in the URL
       return film.characters.includes(`https://swapi-api.hbtn.io/api/people/${wedgeAntillesId}/`)
         ? count + 1
         : count;

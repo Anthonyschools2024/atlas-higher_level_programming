d4bdee3b221465af9f60d4ebea8e472af06b7996
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-11-24 22:09:49 -0600

    had to download the request module and change add node-fetch

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
+const fetch = require('node-fetch');
 
-const apiUrl = process.argv[2];
+const apiUrl = process.argv[2]; 
 const wedgeAntillesId = '18';
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const films = JSON.parse(body).results;
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const films = data.results;
     const wedgeAntillesCount = films.reduce((count, film) => {
       return film.characters.includes(`https://swapi-api.hbtn.io/api/people/${wedgeAntillesId}/`)
         ? count + 1
         : count;
     }, 0);
     console.log(wedgeAntillesCount);
-  }
-});
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
